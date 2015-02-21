599d97d8a66ffef6db6d8dcec82c8205d2419d91
Hustada <hustada80@gmail.com>
2015-02-21 11:15:45 -0600

    Changed config.eager_load to false for production
    Need to see if this might pin point the issue between production and
    development

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150117233904) do
+ActiveRecord::Schema.define(version: 20141218025350) do
 
   create_table "books", force: true do |t|
     t.string   "title"
